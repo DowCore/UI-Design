d23fec0b84e68b9ee2d399796e50cffc01eac44e
dowcore <935693215@qq.com>
2020-12-01 20:07:47 +0800

    feat: add new content

--- demo/resize/src/components/DraggableResizableEditorItem.vue
+++ demo/resize/src/components/DraggableResizableEditorItem.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="editor">
-    <el-row style="height:250px;">
-      <el-col>
-        <div class="selectedTable">
-          <TableEditor v-for="(item,index) in selectedTables" :key="item.uid" :resizor="{ x:5+index*150 }" :uid="item.uid" :id="item.id" :name="item.name" :alias="item.alias" :columns="item.child"></TableEditor>
-        </div>
-      </el-col>
-    </el-row>
-    <el-row style="top:260px;">
-      <el-col>
-        <div class="my">
-          <EditorPanel></EditorPanel>
-        </div>
-      </el-col>
-    </el-row>
-   <!-- <el-row>
+    <el-container>
+      <el-aside width="200px">
+         这是左侧
+      </el-aside>
+      <el-container>
+        <el-header>
+        <TableEditor
+            v-for="(item, index) in selectedTables"
+            :key="item.uid"
+            :resizor="{ x: 5 + index * 150 }"
+            :uid="item.uid"
+            :id="item.id"
+            :name="item.name"
+            :alias="item.alias"
+            :columns="item.child"
+          ></TableEditor></el-header>
+        <el-main> <EditorPanel></EditorPanel></el-main>
+      </el-container>
+    </el-container>
+    <!-- <el-row>
       <DrapResize :x="200" :y="50" :minw="150" :minh="100" :w="300" :h="200">
         <Header
           :header-name="input"
@@ -27,18 +32,13 @@
         ></Body>
       </DrapResize>
     </el-row>-->
-    <el-row>
-      <el-button type="primary" @click="openDialog">打开弹窗</el-button>
-      <Dialog v-model="isOpen"></Dialog>
-    </el-row>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
 import DrapResize from "./DragResize";
-import Dialog from "./Dialog";
-import { store ,mution } from "./SharedModel";
+import { store, mution } from "./SharedModel";
 import EditorPanel from "./EditorPanel";
 import TableEditor from "./TableEditor";
 export default {
@@ -46,9 +46,8 @@ export default {
   props: {},
   watch: {},
   components: {
-    Dialog,
     EditorPanel,
-    TableEditor
+    TableEditor,
   },
   data() {
     return {
@@ -59,18 +58,18 @@ export default {
   },
   created() {
     mution.initTables(this.$store.state.tableList.tables);
-    let list=this.$store.state.tableList.tables.filter((t,index)=>{
-         return  index<2 && !!t;
+    let list = this.$store.state.tableList.tables.filter((t, index) => {
+      return index < 2 && !!t;
     });
-    list.forEach(t=>{
+    list.forEach((t) => {
       mution.SelectedTable(t);
     });
   },
   mounted() {},
   computed: {
-      selectedTables(){
-          return store.SelectedTables;
-      }
+    selectedTables() {
+      return store.SelectedTables;
+    },
   },
   methods: {
     headerChange(val) {
--- 易查笔记/es6.md
+++ 易查笔记/es6.md
@@ -1,1 +1,38 @@
 # {...obj}属于浅拷贝
+# let 
+```javascript
+# 运行正常
+let i=3;
+for(let i=0;i<10;i++){
+
+}
+for (let i = 0; i < 3; i++) {
+  let i = 'abc';
+  console.log(i);
+}
+# 输出3次 abc
+# 循环体内部是一个单独的子作用域
+
+# 异常
+let i=3;
+for(var i=0;i<10;i++){
+
+}
+var i=3;
+for(let i=0;i<10;i++){
+
+}
+
+console.log(bar); // 报错ReferenceError
+let bar = 2;
+
+var tmp = 123;
+
+if (true) {
+  tmp = 'abc'; // ReferenceError
+  let tmp;
+}
+# var 提升变量
+# let不会 
+
+```
